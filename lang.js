b43e73ce5db07c0d91ab130f4b0e957a7030129c
laurion <klauriondareich@gmail.com>
2019-10-21 19:10:01 +0100

    création d'une fonction qui stocke le caractère de la langue dans un cookie

--- lang.js
+++ lang.js
@@ -1,16 +1,18 @@
-//  Cette fonction recupère le path vers le fichier en.json ou fr.json
+//  Cette fonction crée un cookie et stocke le string 'en' ou 'fr'
 
-  function getLanguage(lang){
-    const fullPath = "language/" + lang + '.json';
-
-    retrievedata(fullPath);
+  function setCookie(lang){
+    document.cookie = "lang=" + lang + ';path=/';
+    console.log(document.cookie);
   }
 
 // Cette fonction recupère les données contenues dans le fichier de la langue dont le path a été passé en paramètre
 
-  async function retrievedata(fullPath){
-    langData = fullPath;
-    const response = await fetch(langData);
+  async function retrieveData(){
+    const language = JSON.stringify(document.cookie);
+    const fullPath = "language/" + language + '.json';
+    console.log('le string : ' + fullPath);
+
+    const response = await fetch(fullPath);
     const jsonFormat = await response.json()
     .then(data => {
       getTags(data);
@@ -18,17 +20,15 @@
     })
   }
 
+function getLanguage(lang){
+  const langCharacter = lang;
+  setCookie(langCharacter);
+  retrieveData();
+}
 // Cette fonction cible les éléments du DOM sur lesquels les informations de langue seront affichées
 
 function getTags(data){
   let getData = data;
-
-  // Création du cookie
-
-  setCookie = document.cookie;
-  setCookie = JSON.stringify(getData);
-  console.log(setCookie);
-
   document.getElementById("first").innerHTML = getData.fullname;
   document.getElementById("second").innerHTML = getData.firstname;
   document.getElementById("third").innerHTML = getData.lastname;
