5b915dec8c03700f6a750caa6e9783ae2c60f4a6
laurion <klauriondareich@gmail.com>
2019-10-24 15:36:54 +0100

    ajout de la fonction getCookie et de window.onbeforeunload event, modification de la fonction getLanguage

--- lang.js
+++ lang.js
@@ -2,15 +2,32 @@
 
   function setCookie(lang){
     document.cookie = "lang=" + lang + ';path=/';
-    console.log(document.cookie);
+    console.log("dans le cookie " + document.cookie);
+  }
+
+//  Cette fonction recupère les cookies
+
+  function getCookie() {
+    let cookies = document.cookie.split(';');
+    console.log("acces cookies " + cookies);
+
+    for(let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i];
+      cookie = cookie.substring(1);
+      if(cookie === 'lang=en' || cookie === 'lang=fr'){
+          console.log(cookie);
+          retrieveData(cookie);
+      }
+    }
   }
 
 // Cette fonction envoie une requête AJAX qui recupère les données de langue
 
-  async function retrieveData(){
-    let langCookie = document.cookie.split('=');
+  async function retrieveData(cookie) {
+
+    let langCookie = cookie.split('=');
     let languageChar = langCookie[1];
-    console.log('mychar :  ' + languageChar); // Affichage
+    console.log('mychar +++:  ' + languageChar); // Affichage
 
     let fullPath = "languages/" + languageChar + '.json';
     console.log('chemin du fichier : ' + fullPath); // Affichage
@@ -19,16 +36,16 @@
     const jsonFormat = await response.json()
     .then(data => {
       getTags(data);
-      console.log(data);
+      console.log("mes données : " + data);
     })
   }
 
 // Cette fonction est appelé par un event click et passe en argument le caractère de la langue correspondant
 
-  function getLanguage(langChar){
-    const getChar = langChar;
-    setCookie(getChar);
-    retrieveData();
+  function getLanguage(langChar) {
+   const getChar = langChar;
+   setCookie(getChar);
+   getCookie();
   }
 
 // Cette fonction cible les éléments du DOM sur lesquels les informations de langue seront affichées
@@ -39,3 +56,6 @@ function getTags(data){
   document.getElementById("second").innerHTML = getData.firstname;
   document.getElementById("third").innerHTML = getData.lastname;
 }
+
+// Cette fonction est appelé lorque la page est rafraîchit
+window.onbeforeunload = getCookie();
